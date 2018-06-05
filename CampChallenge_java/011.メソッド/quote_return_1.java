cc37a4af7cdb5e9d4f7b900dfc78f7c8c5b0b760
小田亜味 <amisky31@gmail.com>
2018-06-05 14:45:15 +0900

    「引数と戻り値　引数、戻り値１」の課題を提出します。

---
+++ CampChallenge_java/011.メソッド/quote_return_1.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Ami
+ */
+public class quote_return_1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    
+    String[] prof1 (){
+        String[] prof1 = {"1","01/01","東京"};
+        return prof1;
+    }
+    
+    String[] prof2 (){
+        String[] prof2 = {"2","02/04","大阪"};
+        return prof2;
+    }
+    
+    String[] prof3 (){
+        String[] prof3 = {"3","05/12","広島"};
+        return prof3;
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+        String[] return_prof1 = prof1();
+        String[] return_prof2 = prof2();
+        String[] return_prof3 = prof3();
+        
+        String id = "3";
+        
+        if(id == return_prof1[0]){
+            
+            int i = 0;
+            for(i=0;i<=2;i++){
+                out.print(return_prof1[i]);
+                out.print(" ");
+            }
+            
+        }else if(id == return_prof2[0]){
+            
+            int i = 0;
+            for(i=0;i<=2;i++){
+                out.print(return_prof2[i]);
+                out.print(" ");
+            }
+            
+        }else{
+            
+            int i = 0;
+            for(i=0;i<=2;i++){
+                out.print(return_prof3[i]);
+                out.print(" ");            
+            }
+
+                    
+        }
+        
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
