028d6b2dc90121db0a36af30643d0d28b8c0231c
小田亜味 <amisky31@gmail.com>
2018-06-12 12:54:32 +0900

    「文字列編　バイト数と文字数の取得」の課題を提出します。

---
+++ CampChallenge_java/015.標準クラス/String1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ami
+ */
+public class String1 {
+    
+    public static void main(String[] args){
+        
+        //指名の情報
+        String name = "小田亜味";
+        
+        //バイト数を表示
+        System.out.println("バイト数：　" + name.getBytes().length);
+        //文字数を表示
+        System.out.println("文字数：　" +name.length());
+        
+    }
+    
+}
