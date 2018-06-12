67e167051a99ba347fac0824ecd22369aaaf580a
小田亜味 <amisky31@gmail.com>
2018-06-12 13:03:48 +0900

    「文字列編　文字数の取得」の課題を提出します。

---
+++ CampChallenge_java/015.標準クラス/String2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ami
+ */
+public class String2 {
+    
+    public static void main(String[] args){
+        
+        //メールアドレス情報の取得
+        String mail = "amioda@gmail.com";
+        
+        //＠以降の文字列の文字数を表示
+        System.out.println(mail.substring(7).length());
+        
+    }
+    
+}
